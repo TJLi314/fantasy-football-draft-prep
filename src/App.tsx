549687c83aff60afc8e0535d8dd449b3607f35d6
TJ Li <TianjianLi314@gmail.com>
2024-07-07 18:52:37 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>It's time to make a fantasy draft board</div>
+    </>
+  );
+}
+
+export default App;
